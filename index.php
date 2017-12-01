5bffbf149375ce45a6d6da84eb93e0e0eb126e2c
Lucas Martins Valladares Ribeiro <lucasmbtos@gmail.com>
2017-12-01 11:51:53 -0200

    Webservice inicial teste

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-<?php echo "HELLO WORLD"; ?>
+<?php
+header('Content-Type: application/json');
+
+function processar($att){
+	if($att['result']['action'] == ""){
+		//retorna resposta
+		envia(array(
+			"source" 		=> $att['result']['source'],
+			"speech" 		=> $att['result']['parameters']['msg'],
+			"displayText"	=> ".........Texto..........",
+			"contextOut"	=> array();
+		));
+	}
+}
+
+function envia($parametros){	
+	echo json_encode($parametros);
+}
+
+$att_resposta = file_get_contents("php://input");
+$att = json_decode($att_resposta, true);
+if(isset($update['result']['action'])){
+	processar($att);
+}
+?>
