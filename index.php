b75392a0f59b409ddf9f7e5051e0e69063616872
Lucas Martins Valladares Ribeiro <lucasmbtos@gmail.com>
2017-12-02 02:29:04 -0200

    tratamento dos parametros

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 $method = $_SERVER['REQUEST_METHOD'];
-return $method;
+
 //Processa apenas se for post
 if($method == "POST"){
 	$requisicao = file_get_contents('php://input');
@@ -9,20 +9,18 @@ if($method == "POST"){
 
 	$resposta = new \stdClass();
 
-	return $json->result->parameters['tipo'];
-
-	$alimento-origem = $json->result->parameters['alimento-origem'];
-	$alimento-destino = $json->result->parameters['alimento-destino'];
+	$alimento_origem = $json->result->parameters['alimento-origem'];
+	$alimento_destino = $json->result->parameters['alimento-destino'];
 	$quant = $json->result->parameters['quant'];
 	$tipo = $json->result->parameters['tipo'];
 
 	$parametros = [];
 
-	if(isset($alimento-origem)){
-		$parametros['alimento-origem'] = $alimento-origem;
+	if(isset($alimento_origem)){
+		$parametros['alimento-origem'] = $alimento_origem;
 	}
-	if(isset($alimento-destino)){
-		$parametros['alimento-destino'] = $alimento-destino;
+	if(isset($alimento_destino)){
+		$parametros['alimento-destino'] = $alimento_destino;
 	}
 	if(isset($quant)){
 		$parametros['quant'] = $quant;
@@ -31,7 +29,7 @@ if($method == "POST"){
 		$parametros['tipo'] = $tipo;
 	}
 
-	if(!isset($alimento-origem)){
+	if(!isset($alimento_origem)){
 		$resposta->speech = "Falta nome de alimento";
 		$resposta->displayText = "Você poderia inserir o nome do alimento origem que deseja?";
 		$resposta->source = "webhook";
@@ -40,7 +38,7 @@ if($method == "POST"){
 		$resposta->contextOut['parameters'] = $parametros;
 		return json_encode($resposta);
 	}
-	if(!isset($alimento-destino)){
+	if(!isset($alimento_destino)){
 		$resposta->speech = "Falta nome de alimento";
 		$resposta->displayText = "Você poderia inserir o nome do alimento destino que deseja?";
 		$resposta->source = "webhook";
